a62cfcc49f1dfa23e624cb41a8d0a8da5bec73fe
Frankie Dintino <fdintino@theatlantic.com>
2015-05-06 13:39:28 -0400

    Use setuptools & find_packages; add MANIFEST.in; bump to v2.0.13

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
 
 setup(
     name='Partner Feeds',
     version=__import__('partner_feeds').__version__,
     author_email='ATMOprogrammers@theatlantic.com',
-    packages=['partner_feeds'],
+    packages=find_packages(),
     url='https://github.com/theatlantic/django-partner-feeds',
     description='Consume partner RSS or ATOM feeds',
     long_description=open('README.rst').read(),
+    include_package_data=True,
     install_requires=[
         "Django >= 1.2",
         "celery >= 2.1.4",
